395ecbc3a1b284078ea5c3e6a7a5555806d541f9
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-05-03 14:04:43 -0400

    Added Challenge Gist

---
+++ ruby-fundamentals/bar/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bar::Application.config.secret_token = '0ce0b7f83a7d5a4ff9f637af078f1ebeb5ac117c6c26960583967ad0ac455a927caa106dfb36e356123c9505b5ef991a2b6481766d8ee68f2b8486e0737fe176'
