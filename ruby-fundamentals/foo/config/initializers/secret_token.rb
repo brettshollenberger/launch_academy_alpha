54cc19af40a367f953c653a8107197521f249248
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-05-03 00:15:28 -0400

    First commit

---
+++ ruby-fundamentals/foo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foo::Application.config.secret_token = 'cbed6863897731cea63f4c2fb2aff00a5dc32609687906d11b4107f05527c20284a08454480c58bfb0936d021d00b06e58a150266b162650343eacf39af17bb3'
